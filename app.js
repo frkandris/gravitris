17f70989c793e73d81b9f3b128eb9c6b27bb7637
frkandris <p.toth.andras@gmail.com>
2019-09-14 20:33:28 +0200

    introducing nodemon and browserify

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var browserify = require('browserify-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/javascripts/bundle.js', browserify('./client/main.js'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
