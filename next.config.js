96625b7ce5050d6bc69f871ea77bbb63f2807cdf
frkandris <p.toth.andras@gmail.com>
2025-10-22 23:53:10 +0200

    Remove webpack config, use Turbopack with Prisma externals

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        '@prisma/client': 'commonjs @prisma/client',
-      })
-    }
-    return config
-  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
   },
