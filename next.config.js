5144bb599122d85e83fa9ac772f8950f3c831d1e
frkandris <p.toth.andras@gmail.com>
2025-10-22 23:51:45 +0200

    Fix Prisma binary bundling for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@prisma/client': 'commonjs @prisma/client',
+      })
+    }
+    return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', '@prisma/engines'],
+  },
 }
 
 module.exports = nextConfig
