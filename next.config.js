f09ee95535a90d78a668c30ee8e7a554c7afc00b
frkandris <p.toth.andras@gmail.com>
2025-10-23 21:46:29 +0200

    Remove webpack config to fix Turbopack production build error

--- next.config.js
+++ next.config.js
@@ -5,16 +5,6 @@ const nextConfig = {
     '/api/**/*': ['./node_modules/.prisma/client/**/*'],
     '/': ['./node_modules/.prisma/client/**/*'],
   },
-  // Prevent race condition errors when package.json is read during writes (dev only)
-  webpack: (config, { isServer, dev }) => {
-    if (!isServer && dev) {
-      config.watchOptions = {
-        ...config.watchOptions,
-        ignored: ['**/node_modules', '**/package.json', '**/package-lock.json'],
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
