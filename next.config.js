c9c7adb71d418a90500c0d21f8f68e2ca995906e
frkandris <p.toth.andras@gmail.com>
2025-10-23 21:21:57 +0200

    Fix Next.js config warnings and prevent package.json race condition

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracing: true,
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./node_modules/.prisma/client/**/*'],
-      '/': ['./node_modules/.prisma/client/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/': ['./node_modules/.prisma/client/**/*'],
+  },
+  // Prevent race condition errors when package.json is read during writes
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        ignored: ['**/node_modules', '**/package.json', '**/package-lock.json'],
+      }
+    }
+    return config
   },
 }
 
